ba94683735141e9ec4713911263057ea09332f4d
minh nguyen <hichic375@gmail.com>
2022-07-11 11:35:42 +0700

    Create Account.java

---
+++ Practical Exam/PE1/PE1/Q2_HE151002/src/java/model/Account.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Account {
+    private String name, password, displayname;
+
+    public Account(String name, String password, String displayname) {
+        this.name = name;
+        this.password = password;
+        this.displayname = displayname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+    
+    
+    
+}
