4f529375de157054c7bc26ec2ad4914a3c8d7233
minh nguyen <hichic375@gmail.com>
2022-07-11 11:36:23 +0700

    Create DBContext.java

---
+++ Practical Exam/PE1/PE1/Q1_HE151002/src/java/context/DBContext.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class DBContext {
+     /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+ /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+    public Connection getConnection() throws Exception {
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+
+    /*Insert your other code right after this comment*/
+ /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
+    private final String serverName = "DESKTOP-PRU915R\\DBI";
+    private final String dbName = "PRJ321_FALL_2019_B5";
+    private final String portNumber = "1433";
+    private final String userID = "sa";
+    private final String password = "123";
+
+    public static void main(String[] args) {
+        try {
+            DBContext dBContext = new DBContext();
+            if (dBContext.getConnection() != null) {
+                System.out.println("Connect DB successful");
+            } else {
+                System.out.println("Connect db false");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
